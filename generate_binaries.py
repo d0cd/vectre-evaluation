920a1223a34d1a0d7f2fad2c123337627c72f264
d0cd <pranavsaig@gmail.com>
2021-05-27 15:11:02 -0700

    Script for generating objfiles

---
+++ generate_binaries.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# This script does the following:
+#   1. Clean all files and subdirectories in ./generated-binaries
+#   2. Attempt to generate an object file for each piece of code in ./program-source for each target architecture,
+#      while respecting the directory structure of ./program-source
+
+import os
+import re
+import shutil
+
+bin_dir = './generated-binaries'
+prog_dir = './program-source'
+clang_targets = [
+    'x86_64-unknown-linux-elf',
+    'aarch64-unknown-linux-elf',
+    'mips64-unknown-linux-elf'
+]
+
+
+def generate_objfiles(root, name):
+    if name == '.gitkeep': return
+    file_path = os.path.join(root, name)
+    new_path = file_path.replace(prog_dir, bin_dir).replace(".c", ".o")
+    for target in clang_targets:
+        command = f"clang -c -target {target} -o {new_path} {file_path}"
+        print(f"\n\nIssuing command: {command}")
+        os.system(command)
+
+
+if __name__ == '__main__':
+    # Clean  up generated binaries
+    for file in os.listdir(bin_dir):
+        path = os.path.join(bin_dir, file)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            os.remove(path)
+
+    for root, dirs, files in os.walk(prog_dir):
+        for name in files:
+            generate_objfiles(root, name)
+        for name in dirs:
+           dir_path = os.path.join(root, name)
+           new_path = dir_path.replace(prog_dir, bin_dir)
+           if not os.path.exists(new_path):
+               os.makedirs(new_path)
--- program-source/variant1/kocher_ex1.c
+++ program-source/variant1/kocher_ex1.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <stdint.h>
-extern size_t array1_size, array2_size, array_size_mask;
-extern uint8_t array1[], array2[], temp;
+size_t array1_size, array2_size, array_size_mask;
+uint8_t array1[], array2[], temp;
 
 void victim_function_v01(size_t x) {
      if (x < array1_size) {
           temp &= array2[array1[x] * 512];
      }
+}
+
+void main() {
+    // 
 }
