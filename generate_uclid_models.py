09f0cb6ae98b080d9789de7c15efb9c767782d79
Pranav Gaddamadugu <pranavsaig@gmail.com>
2021-07-13 14:57:57 -0700

    Generating UCLID5 models for programs

--- generate_uclid_models.py
+++ generate_uclid_models.py
@@ -17,9 +17,31 @@ def generate_uclid(root, name):
     if name == '.gitkeep': return
     file_path = os.path.join(root, name)
     new_path = file_path.replace(prog_def_dir, uclid_dir).replace(".spec", ".ucl")
-    command = f"./vectre-angr-disasm/vectre-angr-disasm {file_path} --prog-def {new_path}" #TODO: Change
+    inst_def_path = _select_inst_def_path(file_path)
+    plat_def_path = _select_plat_def_path(file_path)
+    command = f"./vectre/vectre/target/debug/vectre {inst_def_path} {plat_def_path} {file_path} -s {new_path}"
     print(f"\n\nIssuing command: {command}")
     os.system(command)
+
+
+def _select_inst_def_path(path):
+    if 'variant1' in path:
+        if 'aarch64' in path:
+            return "inst-defns/aarch64_spectre_v1_instructions_complete.spec"
+        elif 'x86_64' in path:
+            return "inst-defns/x86_64_spectre_v1_instructions.spec"
+
+    raise Exception(f"Can't find appropriate instruction definition file for path: {path}")
+
+
+def _select_plat_def_path(path):
+    if 'variant1' in path:
+        if 'aarch64' in path:
+            return "platform-defns/aarch64_spectre_v1_platform_complete.spec"
+        elif 'x86_64' in path:
+            return "platform-defns/aarch64_spectre_v1_platform_complete.spec"
+
+    raise Exception(f"Can't find appropriate platform definition file for path: {path}")
 
 
 if __name__ == '__main__':
